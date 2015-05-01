30b1127a19defd94f8945ca1455bf4d23abc155f
pratap224 <pratap224@gmail.com>
2015-05-01 17:25:27 +0530

    first commit

---
+++ app/controllers/homepage_controller.rb
@@ -0,0 +1,25 @@
+class HomepageController < ApplicationController
+  
+  def index
+  	@anouncements = Refineryannounce.last(2)
+    @blogpost =  Refblog.last(2)
+    @testimonials = Reftestimonial.all
+    @posts = Refforum.last(6)
+    @social = Refinery::Page.where(:slug => 'home_social_groups').first
+    @users = Member.all
+    @schools = Application.all
+    @newcast = Refnewcast.all
+    @allstates = State.all
+     
+
+    
+    
+    # @breakings = RefineryBreaking.all
+  end
+  def create
+  
+  end
+  def blog
+      
+  end
+end
