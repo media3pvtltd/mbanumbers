074c693878d4f09f8eb8950491475a44bb122a24
pratap224 <pratap224@gmail.com>
2015-05-17 17:37:44 +0530

    latest issues

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,8 +1,7 @@
 class Subscriber < ActiveRecord::Base
-	validates :name, presence: true
+	
 	validates :email, presence: true
 	validates :email, format: /@/
-	validates :subject, presence: true
-	validates :message, presence: true
+	
 
 end
