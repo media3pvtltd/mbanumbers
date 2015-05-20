90705e76e9f6c02e85c2d6ba75224a06bcfedbc9
pratap224 <pratap224@gmail.com>
2015-05-20 17:54:58 +0530

    suresh issues

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -6,7 +6,7 @@ class HomepageController < ApplicationController
   	@anouncements = Refineryannounce.last(2)
     @blogpost =  Refblog.last(2)
     @testimonials = Reftestimonial.all
-    @posts = Refforum.last(6)
+    @posts = Refforum.last(7)
     @social = Refinery::Page.where(:slug => 'home_social_groups').first
     @users = Member.all
     @schools = Application.all
